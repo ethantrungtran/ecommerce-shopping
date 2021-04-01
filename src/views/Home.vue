bdc81a4ee84014e331d0ba731b61de67ce79099d
trungtd2 <trungtd@tokyotechlab.com>
2021-04-01 16:26:21 +0700

    add view for page web

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <HomeComponent />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HomeComponent from "@/components/home/Index";
+
+export default {
+  name: "Home",
+  components: {
+    HomeComponent,
+  },
+};
+</script>
